dc180a425ac38aa78ec0a2b58d9373905f51ebc0
Gao Haojia <2723687563@qq.com>
2025-06-10 15:27:37 +0800

    Add configuration file for robot communication parameters and update launch file

--- src/robot_communication/launch/robot_communication.launch.py
+++ src/robot_communication/launch/robot_communication.launch.py
@@ -4,15 +4,12 @@ from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, Time
 from launch.launch_description_sources import PythonLaunchDescriptionSource, FrontendLaunchDescriptionSource
 from launch_ros.actions import Node, PushRosNamespace
 from launch.substitutions import LaunchConfiguration
+import os
 
 def generate_launch_description():
     robot_count = LaunchConfiguration('robot_count')
-    network_port = LaunchConfiguration('network_port')
-    network_ip = LaunchConfiguration('network_ip')
 
     declare_robot_count = DeclareLaunchArgument('robot_count', default_value='3', description='')
-    declare_network_port = DeclareLaunchArgument('network_port', default_value='12130', description='')
-    declare_network_ip = DeclareLaunchArgument('network_ip', default_value='192.168.31.87', description='')
 
     robot_communication_node = Node(
         package='robot_communication',
@@ -21,17 +18,19 @@ def generate_launch_description():
         output='screen',
         respawn=True,
         parameters=[{
-            'robot_count': robot_count,
-            'network_port': network_port,
-            'network_ip': network_ip,
-        }]
+                'robot_count': robot_count,
+            },
+            os.path.join(
+                get_package_share_directory("robot_communication"),
+                "config",
+                "robot_communication_params.yaml",
+            ),
+        ],
     )
 
     ld = LaunchDescription()
     
     ld.add_action(declare_robot_count)
-    ld.add_action(declare_network_port)
-    ld.add_action(declare_network_ip)
     
     ld.add_action(robot_communication_node)
     return ld
